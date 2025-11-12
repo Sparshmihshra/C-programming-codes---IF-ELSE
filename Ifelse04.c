546da16f8d497ba16c816631006dcc533297af5f
Sparshmihshra <mishrasparsh159@gmail.com>
2025-11-12 07:19:27 +0530

    Create Ifelse04.c

---
+++ Ifelse04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+        int main()
+            {
+                int a;
+                printf("Enter a number :");
+              scanf("%d",&a);
+
+                if (a%7==0)
+                {
+                    printf("The number is divisable by 7");
+                }
+                else
+                {
+                     printf("The number is not divisible by 7");
+                }
+
+                return 0;
+
+            }
